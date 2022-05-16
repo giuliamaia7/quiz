f79aabdb9aca69736d2a24425253bdc9173e14ff
Giulia Maia <maiagiulia10@gmail.com>
2022-05-16 16:22:40 -0300

    Apresentacao.js

--- src/Apresentacao.js
+++ src/Apresentacao.js
@@ -1,5 +1,15 @@
 import Quiz from "./Quiz";
 
+import Marvel from "./Marvel";
+
+import Lucifer from "./Lucifer";
+
+import Bruxa from "./Bruxa";
+
+import Conhecimento from "./Conhecimento";
+
+import Fofoca from "./Fofoca";
+
 const Apresentacao = (props) => {
 
     const alteraTela = props.alteraTela
@@ -11,10 +21,29 @@ const Apresentacao = (props) => {
 
             <h1> Bem-vindo ao quiz </h1>
             <p> Você está pronto para começar? </p>
-            <button onClick={ ()=> alteraTela( <Quiz alteraTela={alteraTela}/> ) }> Começar </button>
+            <button onClick={ ()=> alteraTela( <Quiz alteraTela={alteraTela}/> ) }> Crepúsculo </button>
+            <br/>
+            <br/>
+            <button onClick={ ()=> alteraTela( <Marvel alteraTela={alteraTela}/> ) }> Marvel </button>
+            <br/>
+            <br/>
+            <button onClick={ ()=> alteraTela( <Lucifer alteraTela={alteraTela}/> ) }> Lúcifer </button>
+            <br/>
+            <br/>
+            <button onClick={ ()=> alteraTela( <Bruxa alteraTela={alteraTela}/> ) }> Bruxa </button>
+            <br/>
+            <br/>
+            <button onClick={ ()=> alteraTela( <Conhecimento alteraTela={alteraTela}/> ) }> Conhecimento </button>
+            <br/>
+            <br/>
+            <button onClick={ ()=> alteraTela( <Fofoca alteraTela={alteraTela}/> ) }> Fofoca </button>
         </div>
 
      );
 }
- 
+
 export default Apresentacao;
+
+
+
+
